05cf6cc20961a0d9f2e5b5ae7e06783bf6911255
Fuad-Alkwkabani <fuadh4991@gmail.com>
2024-11-20 16:05:20 +0100

    creat the class person

---
+++ src/main/java/body_imc/person.java
@@ -0,0 +1,22 @@
+package body_imc;
+
+public class person {
+    private double weight; 
+    private double height; 
+
+
+    
+    public person(double weight, double height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+}
